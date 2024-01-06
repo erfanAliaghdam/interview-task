76b921422618faf261300000647fc9124e527576
erfanAliaghdam <80113382+erfanAliaghdam@users.noreply.github.com>
2024-01-06 16:26:07 +0330

    add db index

--- backend/shop/models/product_model.py
+++ backend/shop/models/product_model.py
@@ -10,7 +10,7 @@ class Product(models.Model):
 
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=12, decimal_places=2)
+    price = models.DecimalField(max_digits=12, decimal_places=2, db_index=True)
     slug = models.SlugField(unique=True, max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     stock = models.PositiveIntegerField()
